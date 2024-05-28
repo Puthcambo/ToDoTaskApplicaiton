b89e30ab9ce0bd5f939bf097abd36eba851dbaff
Puthcambo <2023515eav@aupp.edu.kh>
2024-05-28 10:10:31 +0700

    completed 1

--- TodotaskStreamlit.py
+++ TodotaskStreamlit.py
@@ -1,4 +1,3 @@
-# Import necessary libraries
 import streamlit as st
 
 # Define a Node class for the linked list
@@ -14,8 +13,13 @@ class LinkedList:
 
     def add_task(self, task):
         new_node = Node(task)
-        new_node.next = self.head
-        self.head = new_node
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
 
     def remove_task(self, task):
         current = self.head
@@ -41,34 +45,50 @@ class LinkedList:
 
         return tasks
 
+# Workaround to maintain session state
+class _SessionState:
+    def __init__(self):
+        self._task_list = LinkedList()
+
+    def get_task_list(self):
+        return self._task_list
+
+def get_state():
+    if 'state' not in st.session_state:
+        st.session_state['state'] = _SessionState()
+
+    return st.session_state['state']
+
 # Create a Streamlit app
 def main():
     st.title("To-Do List App with Linked List")
 
-    # Initialize a linked list
-    tasks_list = LinkedList()
+    state = get_state()
 
     # Sidebar for adding tasks
     task_input = st.sidebar.text_input("Add Task:")
     if st.sidebar.button("Add"):
         if task_input:
-            tasks_list.add_task(task_input)
+            state.get_task_list().add_task(task_input)
+            st.sidebar.success(f"Task '{task_input}' added!")
 
     # Sidebar for removing tasks
     task_to_remove = st.sidebar.text_input("Remove Task:")
     if st.sidebar.button("Remove"):
         if task_to_remove:
-            tasks_list.remove_task(task_to_remove)
+            state.get_task_list().remove_task(task_to_remove)
+            st.sidebar.success(f"Task '{task_to_remove}' removed!")
 
-    # Main content to display tasks
-    st.write("## Your To-Do List:")
-    tasks = tasks_list.display_tasks()
+    # View tasks button
+    if st.sidebar.button("View Tasks"):
+        st.write("## Your To-Do List:")
+        tasks = state.get_task_list().display_tasks()
 
-    if not tasks:
-        st.write("No tasks yet. Add some tasks using the sidebar!")
+        if not tasks:
+            st.write("No tasks yet. Add some tasks using the sidebar!")
 
-    for i, task in enumerate(tasks, start=1):
-        st.write(f"{i}. {task}")
+        for i, task in enumerate(tasks, start=1):
+            st.write(f"{i}. {task}")
 
 if __name__ == "__main__":
     main()
